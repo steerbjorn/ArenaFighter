5e82cc58b68035d2349cd37a6e9d863b0958ceb7
Steerbjorn <nilleboyswe@gmail.com>
2022-02-15 16:33:43 +0100

    Add files via upload

---
+++ Arena Fighter/Arena Fighter/Arena.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Arena_Fighter
+{
+    class Arena
+    {
+        private List<Players> enemies;
+        private RollingDie die;
+        private Players player;
+
+        public Arena(Players player, List<Players> enemies, RollingDie die)
+        {
+            this.player = player;
+            this.enemies = enemies;
+            this.die = die;
+        }
+
+
+
+        public void FightingTier()
+        {
+            int playerScore = 0;
+            var aliveEnemies = enemies;
+            var Random = new Random();
+            while (aliveEnemies.Count > 0 && player.PlayerAlive())
+            {
+                var randomEnemy = Random.Next(aliveEnemies.Count);
+                var enemy = aliveEnemies.ElementAt(randomEnemy);
+                aliveEnemies.RemoveAt(randomEnemy);
+
+                Console.WriteLine(enemy + " APPEARS, FIGHT!" + "\n");
+                while (player.PlayerAlive() && enemy.PlayerAlive())
+                {
+                    player.PlayerAttack(enemy);
+                    Thread.Sleep(300);
+                    if (enemy.PlayerAlive())
+                    {
+                        enemy.PlayerAttack(player);
+                        Thread.Sleep(300);
+                    }
+                }
+                if (!enemy.PlayerAlive() && player.PlayerAlive())
+                {
+                    aliveEnemies.Remove(enemy);
+                    player.PlayerAddStats();
+                    Console.WriteLine("You defeated " + enemy);
+                    Console.WriteLine("You gained 75HP and 3STR");
+                    playerScore += 100;
+                }
+                if (enemy.PlayerAlive() && !player.PlayerAlive())
+                {
+                    Console.WriteLine("Game Over, your total score is: " + playerScore);
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
